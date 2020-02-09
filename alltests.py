14f5303e528eec544460421f8a34c3014f57e796
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-02-09 18:15:49 +0000

    attribution for all tests

--- alltests.py
+++ alltests.py
@@ -1,6 +1,10 @@
 import unittest
 
 def run_all_tests():
+  ''' 
+  thanks to https://stackoverflow.com/a/40437679/307454
+  '''
+
   loader = unittest.TestLoader()
   start_dir = 'graphtheory'
   suite = loader.discover(start_dir)
